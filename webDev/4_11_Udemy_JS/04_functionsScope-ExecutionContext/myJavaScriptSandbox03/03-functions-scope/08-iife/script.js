b75c97f4cb6208db15d77bafee65ef55430de41a
kwakd <kwakd.oregonstate.edu>
2023-05-31 18:45:28 -0700

    INPROGRESS: 4_11_Udemy_JS
    WIP section 4

---
+++ webDev/4_11_Udemy_JS/04_functionsScope-ExecutionContext/myJavaScriptSandbox03/03-functions-scope/08-iife/script.js
@@ -0,0 +1,15 @@
+(function(){
+    const user = 'John';
+    console.log(user);
+    const hello = () => console.log('Hello from the IIFE');
+    hello();
+})();
+
+(function(name){
+    console.log('Hello ' + name);
+})('Shawn');
+
+(function hello(){
+    console.log('Hello');
+})();
+
---
+++ webDev/4_11_Udemy_JS/04_functionsScope-ExecutionContext/myJavaScriptSandbox03/03-functions-scope/09-function-challenges/script.js
@@ -0,0 +1,51 @@
+// CHALLENGE 1
+function getCelsius(celF)
+{
+    let tempInt = 0;
+    tempInt = (celF - 32) * 5 / 9;
+    return Math.round(tempInt);
+}
+
+console.log(`The temperature is ${getCelsius(50)} \xB0C`);
+
+// SOLUTION 1
+// const getCelsius = (fahrenheit) => {
+//     const celsius = ((fahrenheit - 32) * 5) / 9;
+//     return Math.round(celsius);
+// };
+// console.log(`The temperature is ${getCelsius(32)} \xB0C`);
+
+
+
+
+
+// CHALLENGE 2
+const minMax = (userArray) =>{
+    let min = Math.min(...userArray);
+    let max = Math.max(...userArray);
+    return{
+        min,
+        max,
+    };
+};
+
+console.log(minMax([55, 32, 43, 54, 65, 76, 87, 98, 109]));
+
+// SOLUTION 2
+// function minMax(arr) {
+//     const min = Math.min(...arr);
+//     const max = Math.max(...arr);
+  
+//   return {
+//   min,
+//   max,
+//   };
+//   }
+  
+//   console.log(minMax([55, 32, 43, 54, 65, 76, 87, 98, 109]));
+
+
+
+
+
+// CHALLENGE 3
