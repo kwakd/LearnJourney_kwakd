db16891d567bc8681b9f285ac0c934543ed0699a
kwakd <kwakd.oregonstate.edu>
2023-08-05 14:48:20 -0700

    INPROGRESS: udemy50Proj50Days
    18/50

---
+++ udemy50p50d-html-js/18_backgroundSlider_080523/02_changingSlides/mybackgroundSlider/script.js
@@ -0,0 +1,43 @@
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+let activeSlide = 0;
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++;
+    if(activeSlide > slides.length - 1)
+    {
+        activeSlide = 0;
+    }
+
+    setBGtoBody();
+    setActiveSlide();
+})
+leftBtn.addEventListener('click', () => {
+    activeSlide--;
+    if(activeSlide < 0)
+    {
+        activeSlide = slides.length - 1;
+    }
+
+    setBGtoBody();
+    setActiveSlide();
+})
+
+setBGtoBody();
+
+function setBGtoBody()
+{
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide()
+{
+    slides.forEach(slide => {
+        slide.classList.remove('active');
+    })
+
+    slides[activeSlide].classList.add('active');
+}
